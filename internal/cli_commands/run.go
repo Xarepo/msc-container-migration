6e6c0faf9014001802fab2e53c548d06a9d105e5
Martin Terneborg <martinterneborg@protonmail.com>
2021-01-27 15:07:40 +0100

    Implement interval for pre-dumping

--- internal/cli_commands/run.go
+++ internal/cli_commands/run.go
@@ -1,7 +1,7 @@
 package cli_commands
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/Xarepo/msc-container-migration/internal/runc"
 	"github.com/rs/zerolog/log"
@@ -12,13 +12,36 @@ type Run struct {
 	BundlePath  *string
 }
 
+// Execute the run command.
+//
+// The run command runs (i.e. create and starts) the container using runc. The
+// container is run in a goroutine to not block execution.
+// The function does not return until the container has exited.
 func (cmd Run) Execute() error {
 	log.Trace().Str("BundlePath", *cmd.BundlePath).Str("ContainerId", *cmd.ContainerId).Msg("Executing run command")
-	status, err := runc.Run(*cmd.ContainerId, *cmd.BundlePath)
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-	log.Debug().Str("Status", string(status)).Msg("Run exited")
+	result := make(chan int)
+	go func() {
+		status, err := runc.Run(*cmd.ContainerId, *cmd.BundlePath)
+		if err != nil {
+			log.Error().Str("Error", err.Error()).Msg("Error running container")
+		} else {
+			log.Info().Str("Status", string(status)).Msg("Container exited")
+		}
+		result <- status
+	}()
+
+	// Pre-dump loop
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				log.Trace().Str("ContainerId", *cmd.ContainerId).Msg("Pre-dumping")
+			}
+		}
+	}()
+
+	log.Trace().Msg("Waiting for container to exit")
+	<-result // Wait for container to chanel
 	return nil
 }
