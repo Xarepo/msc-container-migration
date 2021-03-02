aed875c461358bbdb35d39a899d2c43e2ba537f2
Martin Terneborg <martinterneborg@protonmail.com>
2021-03-02 09:52:18 +0100

    Update benchmarking script

--- scripts/benchmarking/storage.sh
+++ scripts/benchmarking/storage.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
 DUMP_DIR=/dumps
 DUMPS=$(ls -l $DUMP_DIR | tail -n +2 | awk '{print $9}')
+FULL_DUMPS=$(echo "$DUMPS" | sed -n '/d[0-9]\+/p')
+PRE_DUMPS=$(echo "$DUMPS" | sed -n '/p[0-9]\+/p')
 
-SUM=0
-for dump in $DUMPS ; do
-	files=$(ls -l $DUMP_DIR/$dump | tail -n +2 | awk '{print $9}')
-	for file in $files ; do
-		size=$(stat $DUMP_DIR/$dump/$file | sed -n '2p' | awk '{print $2}')
-		SUM=$((SUM + size))
+# Sums the size of the dumps. Uses bytes stored rather than entire blocks.
+function sum_dumps() {
+	local SUM=0
+	for dump in $1 ; do
+		files=$(ls -l $DUMP_DIR/$dump | tail -n +2 | awk '{print $9}')
+		for file in $files ; do
+			size=$(stat $DUMP_DIR/$dump/$file | sed -n '2p' | awk '{print $2}')
+			SUM=$((SUM + size))
+		done
 	done
-done
+	echo $SUM
+}
 
-echo SUM: $SUM
+SUM_PRE=$(sum_dumps "$PRE_DUMPS")
+NUM_PRE=$(echo "$PRE_DUMPS" | wc -l)
+AVG_PRE=$((SUM_PRE/NUM_PRE))
+
+SUM_FULL=$(sum_dumps "$FULL_DUMPS")
+NUM_FULL=$(echo "$FULL_DUMPS" | wc -l)
+AVG_FULL=$((SUM_FULL/NUM_FULL))
+
+echo SUM_PRE=$SUM_PRE
+echo NUM_PRE=$NUM_PRE
+echo AVG_PRE=$AVG_PRE
+
+echo
+
+echo SUM_FULL=$SUM_FULL
+echo NUM_FULL=$NUM_FULL
+echo AVG_FULL=$AVG_FULL
